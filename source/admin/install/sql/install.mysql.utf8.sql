12a5d81fdfce75fe2895adc58340a9300d4c5b64
gaurav <gaurav@gaurav.(none)>
2013-07-08 23:37:54 +0530

    fields tble added

---
+++ source/admin/install/sql/install.mysql.utf8.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE IF NOT EXISTS `#__cscommunity_field` (
+  `field_id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `tooltip` text NOT NULL,
+  `parent` int(10) DEFAULT NULL,
+  `type` varchar(255) NOT NULL,
+  `ordering` int(11) DEFAULT '0',
+  `published` tinyint(1) NOT NULL DEFAULT '0',  
+  `required` tinyint(1) DEFAULT '0',
+  `registration` tinyint(1) DEFAULT '1',
+  `options` text,
+  `code` varchar(255) NOT NULL,
+  `params` text NOT NULL,
+  PRIMARY KEY (`field_id`),
+  KEY `fieldcode` (`code`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
